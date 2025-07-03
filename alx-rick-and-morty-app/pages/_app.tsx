40f58b4cf20d7d0ec55c95f5171a810dca3cd9f6
Vegas90 <amosmwoni@gmail.com>
2025-07-03 14:23:03 +0300

    m

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,17 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 // Import Apollo Provider to wrap your whole app
-import { ApolloProvider } from "@apollo/client";
+//import { ApolloProvider } from "@apollo/client";
 
 // Import the Apollo client you just created
-import client from "@/graphql/apolloClient";
+//import client from "@/graphql/apolloClient";
+
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 // Main app component
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <ErrorBoundary>  
+       <Component {...pageProps} />
+       
+       </ErrorBoundary>
     // Make Apollo client available throughout the app
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+   //  <ApolloProvider client={client}>
+      // <Component {...pageProps} />  
+    // </ApolloProvider>
+    
   );
 }
